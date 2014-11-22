b743e0887dcb2a87e6f5ae99a7fb369d17517bcc
Saira J. Barlas <c12809080@mydit.ie>
2014-11-22 15:15:25 +0000

    Update Input.c

--- Input.c
+++ Input.c
@@ -1,25 +1,25 @@
-#include <stdio.h>
+#include <stiod.h>
 main()
 {
   int a, b, c;
-	
-	printf("Please insert a: \n");
-	scanf("%d", &a);
-	
-	printf("Please insert b: \n");
-	scanf("%d", &b);
-	
-	c = a*b;
-	
-	printf("The product of the above 2 is %d\n", c);
-	
-	if(c > 100)
-	{
-		printf("\nYou're ahead of the schedule!");
-	} // end if
-	
-	else
-	{
-		printf("\nThis really is what you got!");
-	} // end else
+  
+  printf("Please insert a: \n");
+  scanf("%d", &a);
+  
+  printf("Please insert b: \n");
+  scanf("%d", &b);
+  
+  c = a*b;
+  
+  printf("The product of the above 2 is %d\n", c);
+  
+  if(c > 100)
+  {
+    printf("\nYou're ahead of the schedule!");
+  } // end if
+  
+  else
+  {
+    printf("\nThis really is what you got!");
+  } // end else
 } // end main

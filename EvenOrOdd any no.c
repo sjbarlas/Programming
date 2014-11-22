ce1d369651b89f4f82d08258f478822adff97a41
Saira J. Barlas <c12809080@mydit.ie>
2014-11-22 19:39:57 +0000

    Create EvenOrOdd any no.c

---
+++ EvenOrOdd any no.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+main()
+{
+  int a, i;
+  
+  printf("Please put in a number you would like to test: ");
+  scanf("%d", &a);
+  
+  for(i = 0; i <= a; i++)
+  {
+    if(a % 2 == 0)
+	{
+	  printf("\nYour number is even!\n");
+	  break;
+	} // end if
+	
+	else
+	{
+	  printf("\nYour number is odd!\n");
+	  break;
+	} //end else
+  } // end for
+} // end main
+

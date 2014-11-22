64cc2b07ad07428aeeb432692cd45ab37370f64e
Saira J. Barlas <c12809080@mydit.ie>
2014-11-22 15:08:41 +0000

    Create Input.c

---
+++ Input.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+main()
+{
+	int a, b, c;
+	
+	printf("Please insert a: \n");
+	scanf("%d", &a);
+	
+	printf("Please insert b: \n");
+	scanf("%d", &b);
+	
+	c = a*b;
+	
+	printf("The product of the above 2 is %d\n", c);
+	
+	if(c > 100)
+	{
+		printf("\nYou're ahead of the schedule!");
+	}
+	
+	else
+	{
+		printf("\nThis really is what you got!");
+	}
+}

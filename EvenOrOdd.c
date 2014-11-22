baf04f2762ccf590c6c5e1999cfc462268a475b7
Saira J. Barlas <c12809080@mydit.ie>
2014-11-22 19:24:37 +0000

    Update EvenOrOdd.c

--- EvenOrOdd.c
+++ EvenOrOdd.c
@@ -1,43 +1,43 @@
 #include <stdio.h>
 main()
 {
-	int a, b, integer;
-	
-	printf("Please insert 2 numbers that you would like to check if it's even or odd\n");
-	
-	printf("\n\n1: ");
-	scanf("%d", &a);
-	
-	printf("\n\n2: ");
-	scanf("%d", &b);
-	
-	for(integer = 0; integer <= a; integer++)
-	{
-		if(a % 2 == 0)
-		{
-			printf("\n\nYour first number is even!\n");
-			break;
-		}
-		
-		else
-		{
-			printf("\n\nYour first number is odd!\n");
-			break;
-		}
-	}
-	
-	for(integer = 0; integer <= b; integer++)
-	{
-		if(b % 2 == 0)
-		{
-			printf("\n\nYour second number is even!\n");
-			break;
-		}
-		
-		else
-		{
-			printf("\n\nYour second number is odd!\n");
-			break;
-		} 
-	} //end for
+  int a, b, integer;
+
+  printf("Please insert 2 numbers that you would like to check if it's even or odd\n");
+
+  printf("\n\n1: ");
+  scanf("%d", &a);
+
+  printf("\n\n2: ");
+  scanf("%d", &b);
+
+  for(integer = 0; integer <= a; integer++)
+  {
+    if(a % 2 == 0)
+    {
+      printf("\n\nYour first number is even!\n");
+      break;
+    }
+
+    else
+    {
+      printf("\n\nYour first number is odd!\n");
+      break;
+    }
+  }
+
+  for(integer = 0; integer <= b; integer++)
+  {
+    if(b % 2 == 0)
+    {
+      printf("\n\nYour second number is even!\n");
+      break;
+    }
+
+    else
+    {
+      printf("\n\nYour second number is odd!\n");
+      break;
+    } 
+  } //end for
 } //end main

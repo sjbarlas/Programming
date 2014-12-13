5f8fed66ade961fcbdcea79dc796df96ceaf799d
S J. Barlas <c12809080@mydit.ie>
2014-12-13 12:38:53 +0000

    Update Compare.c

--- Programs/Compare.c
+++ Programs/Compare.c
@@ -4,11 +4,37 @@
 main()
 {
   int a;
+	
   printf("Enter number\n");
   scanf("%d", &a);
-  
-  if(a > 10)
-  {
-  	printf("The number you entered %d, is greater than 10", a);
-  }
+	
+  if(a >= 10 && a < 20)
+    {
+        printf("The number you entered %d, is greater than 10", a);
+    }
+	
+  else if(a >= 20 && a < 30)
+    {
+        printf("%d = Wow, your number is greater than 20!", a);
+    }
+	
+  else if(a >= 30 && a < 40)
+    {
+        printf("%d = Aiming high are we?", a);
+    }
+	
+  else if(a >= 40 && a < 80)
+    {
+        printf("%d = My my", a);
+    }
+	
+  else if(a >= 80 && a < 100)
+    {
+        printf("%d = We're getting there!", a);
+    }
+	
+  else if(a >= 100)
+    {
+       printf("%d = You've done it!", a);
+    }
 }

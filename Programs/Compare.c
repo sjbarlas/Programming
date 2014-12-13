e159056885b0fefb0fb43274c6508b5914632ada
S J. Barlas <c12809080@mydit.ie>
2014-12-13 11:52:21 +0000

    Create Compare.c

---
+++ Programs/Compare.c
@@ -0,0 +1,15 @@
+/* Prints if number > 10 */
+
+#include <stdio.h>
+main()
+{
+	int a;
+	
+	printf("Enter number\n");
+	scanf("%d", &a);
+	
+	if(a > 10)
+	{
+		printf("The number you entered %d, is greater than 10", a);
+	}
+}

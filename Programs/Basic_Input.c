827cad2b6ca52d694360726282ebb229226e177e
S J. Barlas <c12809080@mydit.ie>
2014-12-13 11:49:35 +0000

    Create Basic_Input.c

---
+++ Programs/Basic_Input.c
@@ -0,0 +1,12 @@
+/* Program to display input */
+
+#include <stdio.h>	
+main()
+{
+  int a;
+		
+  printf("Enter a number\n");
+  scanf("%d", &a);
+		
+  printf("The number you entered is %d", a);
+}

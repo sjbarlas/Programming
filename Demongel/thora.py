edc79bcc6bd7ba6f413b016516fe0308b4d569e5
Saira J. Barlas <c12809080@mydit.ie>
2015-02-08 13:40:24 +0000

    Update thora.py

--- Demongel/thora.py
+++ Demongel/thora.py
@@ -1,13 +1,12 @@
 def game():
     print("-----HELLO TO THIS GAME!")
-    print("-----WE HAVE BEEN WAITING FOR YOU!")
+    print("-----WE HAVE BEENWAITING FOR YOU!")
     print("-----GET READY FOR THIS JOURNEY!")
     print("-----ARE YOU READY TO PLAY?")
     answer = input("If you are ready, press R. If you don't want to play, press Q:\n").lower()
 
     if answer == 'R' or answer == 'r':
-        
-        # Picking the locations
+    # Picking the locations
        print("WELCOME TO THE GAME!")
        print("You have been teleported to a castle.")
        print("You see:")
@@ -16,21 +15,29 @@ def game():
        print("3 - Market on the LEFT")
        print("Where would you like to go?")
        answer = input("Press the corresponding number to pick a location to travel")
+
+       # Castle
        if answer == '1':
-           # Location #1
            print("The door is locked")
+
+       # Forest
        elif answer == '2':
-           # Location #2
             print("You run into the forest with the big trees.")
+            print("From the corner of your eye you see something which isn't at all pleasing.")
+            print("The skyis dark.")
+            print("You continue walking.")
+
+       # Market     
        elif answer == '3':
-           # Location #3
             print("You have entered the bright market place.")
+
+       # Error checking
        else:
-           # Error checking
             print("Invalid number. Try again!")
             # Need to declare a loop ...
 
-       
+    # Quit
     elif answer == 'Q' or answer == 'q':
-       print("Good Bye!")
+        print("Good Bye!")
+
 game()
